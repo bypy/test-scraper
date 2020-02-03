8b7be0d6f2c0ae069d65686c834167261cc3e1b2
Alexei <mail@mail.by>
2020-02-03 13:29:48 +0300

    switched to jsdom

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const request = require('request');
-let jar = request.jar();
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 let urls = [
     'https://www.domikmod.by/blog/263.html',
     'https://www.domikmod.by/blog/259.html'
 ];
+let jar = request.jar();
 
 const next = function(newCookies) {
     if (urls.length > 0) {
@@ -14,15 +16,20 @@ const next = function(newCookies) {
             jar:(newCookies||jar),
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; …) Gecko/20100101 Firefox/72.0"
         }, function(error, response, body){
+            if (response.statusCode === 200) {
+                const htmlDom = new JSDOM(body);
+                let title = htmlDom.window.document.title;
+            }
             console.log(body.match(/<h1>(.*?)<\/h1>/)[1]);
+
             if (!response.headers["set-cookie"])
                 setTimeout( () => {
                     next(request.jar());
-                }, 2200 );
+                }, (4000 + 4000*Math.random()) );
             else
                 setTimeout( () => {
                     next(response.headers["set-cookie"]);
-                } , 2200 );
+                } , (4000 + 4000*Math.random()) );
         });
     }
 };
