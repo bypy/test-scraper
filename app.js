ddd1c89b095cada0f3ecdf6c4d8884eb8cc4c2a1
Alexei <mail@mail.by>
2020-02-03 19:26:35 +0300

    парсер сохраняет страницы на диск

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
 const request = require('request');
 const jsdom = require("jsdom");
+const fs = require('fs');
+const path = require('path');
 const { JSDOM } = jsdom;
 
 let urls = [
-    'https://www.domikmod.by/blog/263.html',
-    'https://www.domikmod.by/blog/259.html'
+    'https://scanlibs.com/learning-svg/',
+    'https://scanlibs.com/jump-start-sass/'
 ];
-let jar = request.jar();
 
-const next = function(newCookies) {
-    if (urls.length > 0) {
-        request.get(urls.pop(), {
-            timeout: 10000,
-            followRedirect: true,
-            jar:(newCookies||jar),
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; …) Gecko/20100101 Firefox/72.0"
-        }, function(error, response, body){
-            if (response.statusCode === 200) {
-                const htmlDom = new JSDOM(body);
-                let title = htmlDom.window.document.title;
-            }
-            console.log(body.match(/<h1>(.*?)<\/h1>/)[1]);
+const rawNext = () => {
+    setTimeout( () => {
+        next();
+    }, (4000 + 4000*Math.random()) );
+}
 
+const next = cookies => {
+    if (!urls || urls.length === 0) {
+        console.log('All urls crawled');
+        return;
+    }
+
+    let currUrl = urls.pop();
+    let fileName = currUrl.replace('https://scanlibs.com/', '').split('/')[0];
+
+    let reqOpts = {
+        timeout: 10000,
+        followRedirect: true,
+        jar:(cookies||request.jar()),
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
+    };
+
+    request.get(currUrl, reqOpts, (error, response, body) => {
+        if (error) {
+            rawNext();
+        } else if (response.statusCode === 200) {
+            fs.writeFile(path.join(__dirname,'pages',fileName), body, (err) => {
+                if (err) {
+                    console.log(err);
+                    rawNext();
+                }
+            });
+            const htmlDom = new JSDOM(body);
+            let title = htmlDom.window.document.title;
+            console.log(title);
+            
             if (!response.headers["set-cookie"])
-                setTimeout( () => {
-                    next(request.jar());
-                }, (4000 + 4000*Math.random()) );
+                rawNext();
             else
                 setTimeout( () => {
                     next(response.headers["set-cookie"]);
                 } , (4000 + 4000*Math.random()) );
-        });
-    }
+        }
+    });
 };
 
 next();
