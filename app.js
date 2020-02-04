43c0aafca92955f4bdd692d08569339627a88545
Alexei <mail@mail.by>
2020-02-04 19:32:00 +0300

    Сохраняет искомые данные в json

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const { JSDOM } = jsdom;
 
+const pagesPath = path.join(__dirname, 'pages');
+const imgPath = path.join(__dirname, 'images');
+const dataPath = path.join(__dirname, 'data');
+
+[pagesPath, imgPath, dataPath].forEach( pth => {
+    if (!fs.existsSync(pth)){
+        fs.mkdirSync(pth);
+    }  
+})
+
 const siteAddr = 'https://scanlibs.com/'
 let urls = [
     'https://scanlibs.com/learning-svg/',
@@ -33,28 +43,101 @@ const next = cookies => {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
     };
 
+
     request.get(currUrl, reqOpts, (error, response, body) => {
+
         let data = {};
+        let contImgSrc = null;
+        let contImgExt = ""; // изначально расширение изображение неизвестно
+
+        let savePath = "";
+        
         try {
             if (error) throw error;
             if (response.statusCode !== 200) throw new Error('error getting resource ' + currUrl);
-            let savePath = path.join(__dirname, 'pages', fileName.concat('.html'));
+            savePath = path.join(__dirname, 'pages', fileName.concat('.html'));
             fs.writeFile(savePath, body, (err) => {
-                if (err) throw error;
+                if (err) console.log(error);
             });
-            const htmlDom = new JSDOM(body);
-            const document = htmlDom.window.document;
-            const categoryItems = document.querySelector('span.cat-links').children;
+            const html = body.match(/<body.*?>.+<\/body>/s)[0].replace(/<script.*?>[\s\S]*?<\/script>/gs, "");
+            const htmlDom = new JSDOM(html);
+            const htmlBody = htmlDom.window.document.body;
+
+            // Extracting data
+            const name = htmlBody.querySelector('h1').textContent;
+            const categoryItems = htmlBody.querySelector('span.cat-links').children;
             let categories = Array.prototype.slice.call(categoryItems);
             categories = categories
                 .filter(item => item.nodeName === 'A')
                 .map(item => item.textContent)
                 .filter(item => (item));
-            const contImg = document.querySelector('.entry-content > img').getAttribute('src');
-                        
+
+            contImgSrc = htmlBody.querySelector('.entry-content > img').getAttribute('src');
+            if (/\.jpe?g$/.test(contImgSrc))
+                contImgExt = '.jpg';
+            else if (/\.png$/.test(contImgSrc))
+                contImgExt = '.png';
+            else if (/\.webp$/.test(contImgSrc))
+                contImgExt = '.webp';
+
+            const container = htmlBody.querySelector('.entry-content');
+            if (!container) throw new Error('Data containin element is missing');
+            const info = {};
+            const description = [];
+            const descriptionEl = container.querySelector('p');
+            
+           
+            const chldNodes = container.childNodes;
+            let counter=0;
+            while(counter < chldNodes.length) {
+                let curr = chldNodes[counter];
+                if (curr === descriptionEl)
+                    break;
+                if (curr.nodeType === 3 && curr.textContent.trim() !== '') {
+                    let key = '';
+                    let value='';
+                    let properties = curr.textContent.trim().split(':');
+                    key = properties[0];
+                    if (properties.length > 1) {
+                        value = properties.slice(1).join(':').trim(); // если значение в свою очередь само имело знак :
+                    }
+                    info[key] = value;
+                }
+                counter++;
+            }
+
+            const download = container.querySelector('#download');
+
+            if (descriptionEl) {
+                description.push(descriptionEl.innerHTML);
+                let startEl = descriptionEl;
+                while (true) {
+                    let nextSibl = startEl.nextElementSibling;
+                    if (!nextSibl || nextSibl === download)
+                        break;
+                    else {
+                        description.push(nextSibl.innerHTML);
+                        startEl = nextSibl;
+                    }
+                }
+            }
+
+            // TODO:  sanitize description -- leave only p, ul, li
+
+            const tagListEl = htmlBody.querySelectorAll('.entry-meta > .tagcloud > a');
+            const tags = Array.prototype.map.call(tagListEl, item => item.textContent);
+            
+            data.name = name;
             data.cat = categories;
-            data.imgSrc = contImg;
-            data.imgDst = path.join(__dirname, 'images', fileName.concat('.jpg'));
+            data.img = path.join(__dirname, 'images', fileName.concat(contImgExt));
+            data.info = info;
+            data.description = description;
+            data.tags = tags;
+
+            savePath = path.join(__dirname, 'data', fileName.concat('.json'));
+            fs.writeFile(savePath, JSON.stringify(data), (err) => {
+                if (err) throw error;
+            });
 
             if (!response.headers["set-cookie"])
                 noCookiesNext(timeout);
@@ -68,19 +151,13 @@ const next = cookies => {
             noCookiesNext(timeout);
         }
 
-        let contImgExt = null;
-        
         request
             .get(siteAddr.concat(data.imgSrc))
             .on('error', (err) =>
                 console.log(err)
             )
-            .on('response', (response) => {
-                contImgExt = response.headers['content-type'].split('/')[1];
-                data.img = path.join(__dirname, 'images', fileName.concat('.', contImgExt));
-            })
-            .pipe(fs.createWriteStream(data.imgDst));
-        
+            .pipe(fs.createWriteStream(data.img));
+
     });
 };
 
