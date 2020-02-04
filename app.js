041e86c5c505712775f9a99eefa8e67eb62b8d87
Alexei <123@mail.by>
2020-02-04 23:31:31 +0300

    парсер страницы в отдельной функции

--- app.js
+++ app.js
@@ -15,11 +15,102 @@ const dataPath = path.join(__dirname, 'data');
 })
 
 const siteAddr = 'https://scanlibs.com/'
+const timeout = 4000 + 4000*Math.random();
+
 let urls = [
     'https://scanlibs.com/learning-svg/',
     'https://scanlibs.com/jump-start-sass/'
 ];
-const timeout = 4000 + 4000*Math.random();
+
+
+const scrapePage = (htmlBody, pageName) => {
+    let data = {};
+
+    // Extracting data
+    const name = htmlBody.querySelector('h1').textContent;
+    const categoryItems = htmlBody.querySelector('span.cat-links').children;
+    let categories = Array.prototype.slice.call(categoryItems);
+    categories = categories
+        .filter(item => item.nodeName === 'A')
+        .map(item => item.textContent)
+        .filter(item => (item));
+
+    let contImgSrc = htmlBody.querySelector('.entry-content > img').getAttribute('src');
+    let contImgExt = ""; // изначально расширение изображения нам неизвестно
+
+    if (/\.jpe?g$/.test(contImgSrc))
+        contImgExt = '.jpg';
+    else if (/\.png$/.test(contImgSrc))
+        contImgExt = '.png';
+    else if (/\.webp$/.test(contImgSrc))
+        contImgExt = '.webp';
+        
+    const container = htmlBody.querySelector('.entry-content');
+    if (!container) throw new Error('Data containin element is missing');
+    const info = {};
+    const description = [];
+    const descriptionEl = container.querySelector('p');
+    
+   
+    const chldNodes = container.childNodes;
+    let counter=0;
+    while(counter < chldNodes.length) {
+        let curr = chldNodes[counter];
+        if (curr === descriptionEl)
+            break;
+        if (curr.nodeType === 3 && curr.textContent.trim() !== '') {
+            let key = '';
+            let value='';
+            let properties = curr.textContent.trim().split(':');
+            key = properties[0];
+            if (properties.length > 1) {
+                value = properties.slice(1).join(':').trim(); // если значение в свою очередь само имело знак :
+            }
+            info[key] = value;
+        }
+        counter++;
+    }
+
+    const download = container.querySelector('#download');
+
+    if (descriptionEl) {
+        description.push(descriptionEl.innerHTML);
+        let startEl = descriptionEl;
+        while (true) {
+            let nextSibl = startEl.nextElementSibling;
+            if (!nextSibl || nextSibl === download)
+                break;
+            else {
+                description.push(nextSibl.innerHTML);
+                startEl = nextSibl;
+            }
+        }
+    }
+
+    // TODO:  sanitize description -- leave only p, ul, li
+
+    const tagListEl = htmlBody.querySelectorAll('.entry-meta > .tagcloud > a');
+    const tags = Array.prototype.map.call(tagListEl, item => item.textContent);
+    
+    data.name = name;
+    data.cat = categories;
+    data.img = path.join(__dirname, 'images', pageName.concat(contImgExt));
+    data.info = info;
+    data.description = description;
+    data.tags = tags;
+
+    // сохраним картинку
+    request
+        .get(siteAddr.concat(contImgSrc))
+        .on('error', (err) => {
+            throw err;
+        })
+        .pipe(fs.createWriteStream(data.img));
+    
+    return data;
+
+};
+
 
 const noCookiesNext = (delay) => {
     setTimeout(() => {
@@ -27,7 +118,9 @@ const noCookiesNext = (delay) => {
     }, delay);
 };
 
+
 const next = cookies => {
+
     if (!urls || urls.length === 0) {
         console.log('All urls crawled');
         return;
@@ -43,19 +136,15 @@ const next = cookies => {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
     };
 
-
     request.get(currUrl, reqOpts, (error, response, body) => {
-
-        let data = {};
-        let contImgSrc = null;
-        let contImgExt = ""; // изначально расширение изображение неизвестно
-
-        let savePath = "";
         
         try {
-            if (error) throw error;
-            if (response.statusCode !== 200) throw new Error('error getting resource ' + currUrl);
-            savePath = path.join(__dirname, 'pages', fileName.concat('.html'));
+            if (error)
+                throw error;
+            if (response.statusCode !== 200)
+                throw new Error('error getting resource ' + currUrl);
+            
+            let savePath = path.join(__dirname, 'pages', fileName.concat('.html'));
             fs.writeFile(savePath, body, (err) => {
                 if (err) console.log(error);
             });
@@ -63,79 +152,10 @@ const next = cookies => {
             const htmlDom = new JSDOM(html);
             const htmlBody = htmlDom.window.document.body;
 
-            // Extracting data
-            const name = htmlBody.querySelector('h1').textContent;
-            const categoryItems = htmlBody.querySelector('span.cat-links').children;
-            let categories = Array.prototype.slice.call(categoryItems);
-            categories = categories
-                .filter(item => item.nodeName === 'A')
-                .map(item => item.textContent)
-                .filter(item => (item));
-
-            contImgSrc = htmlBody.querySelector('.entry-content > img').getAttribute('src');
-            if (/\.jpe?g$/.test(contImgSrc))
-                contImgExt = '.jpg';
-            else if (/\.png$/.test(contImgSrc))
-                contImgExt = '.png';
-            else if (/\.webp$/.test(contImgSrc))
-                contImgExt = '.webp';
-
-            const container = htmlBody.querySelector('.entry-content');
-            if (!container) throw new Error('Data containin element is missing');
-            const info = {};
-            const description = [];
-            const descriptionEl = container.querySelector('p');
-            
-           
-            const chldNodes = container.childNodes;
-            let counter=0;
-            while(counter < chldNodes.length) {
-                let curr = chldNodes[counter];
-                if (curr === descriptionEl)
-                    break;
-                if (curr.nodeType === 3 && curr.textContent.trim() !== '') {
-                    let key = '';
-                    let value='';
-                    let properties = curr.textContent.trim().split(':');
-                    key = properties[0];
-                    if (properties.length > 1) {
-                        value = properties.slice(1).join(':').trim(); // если значение в свою очередь само имело знак :
-                    }
-                    info[key] = value;
-                }
-                counter++;
-            }
-
-            const download = container.querySelector('#download');
-
-            if (descriptionEl) {
-                description.push(descriptionEl.innerHTML);
-                let startEl = descriptionEl;
-                while (true) {
-                    let nextSibl = startEl.nextElementSibling;
-                    if (!nextSibl || nextSibl === download)
-                        break;
-                    else {
-                        description.push(nextSibl.innerHTML);
-                        startEl = nextSibl;
-                    }
-                }
-            }
-
-            // TODO:  sanitize description -- leave only p, ul, li
-
-            const tagListEl = htmlBody.querySelectorAll('.entry-meta > .tagcloud > a');
-            const tags = Array.prototype.map.call(tagListEl, item => item.textContent);
-            
-            data.name = name;
-            data.cat = categories;
-            data.img = path.join(__dirname, 'images', fileName.concat(contImgExt));
-            data.info = info;
-            data.description = description;
-            data.tags = tags;
+            const pageData = scrapePage(htmlBody, fileName);
 
             savePath = path.join(__dirname, 'data', fileName.concat('.json'));
-            fs.writeFile(savePath, JSON.stringify(data), (err) => {
+            fs.writeFile(savePath, JSON.stringify(pageData), (err) => {
                 if (err) throw error;
             });
 
@@ -150,13 +170,6 @@ const next = cookies => {
             console.log(err);
             noCookiesNext(timeout);
         }
-
-        request
-            .get(siteAddr.concat(data.imgSrc))
-            .on('error', (err) =>
-                console.log(err)
-            )
-            .pipe(fs.createWriteStream(data.img));
 
     });
 };
