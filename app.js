3835aaa80704854d4ad7608db3e3e950250a4951
Alexei <123@mail.by>
2020-02-06 01:10:27 +0300

    cookie handling

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ const pagesPath = path.join(__dirname, 'pages');
 const imgPath = path.join(__dirname, 'images');
 const dataPath = path.join(__dirname, 'data');
 
-[pagesPath, imgPath, dataPath].forEach( pth => {
-    if (!fs.existsSync(pth)){
+[pagesPath, imgPath, dataPath].forEach(pth => {
+    if (!fs.existsSync(pth)) {
         fs.mkdirSync(pth);
-    }  
+    }
 })
 
 const siteAddr = 'https://scanlibs.com/'
-const timeout = 4000 + 4000*Math.random();
+const timeout = 4000 + 4000 * Math.random();
 
 // let urls = [
-    // 'https://scanlibs.com/learning-svg/',
-    // 'https://scanlibs.com/jump-start-sass/'
+// 'https://scanlibs.com/learning-svg/',
+// 'https://scanlibs.com/jump-start-sass/'
 // ];
 
 
@@ -46,23 +46,23 @@ const scrapePage = (htmlBody) => {
         contImgExt = '.png';
     else if (/\.webp$/.test(imgSrc))
         contImgExt = '.webp';
-        
+
     const container = htmlBody.querySelector('.entry-content');
     if (!container) throw new Error('Data containin element is missing');
     const info = {};
     const description = [];
     const descriptionEl = container.querySelector('p');
-    
-   
+
+
     const chldNodes = container.childNodes;
-    let counter=0;
-    while(counter < chldNodes.length) {
+    let counter = 0;
+    while (counter < chldNodes.length) {
         let curr = chldNodes[counter];
         if (curr === descriptionEl)
             break;
         if (curr.nodeType === 3 && curr.textContent.trim() !== '') {
             let key = '';
-            let value='';
+            let value = '';
             let properties = curr.textContent.trim().split(':');
             key = properties[0];
             if (properties.length > 1) {
@@ -93,9 +93,9 @@ const scrapePage = (htmlBody) => {
 
     const tagListEl = htmlBody.querySelectorAll('.entry-meta > .tagcloud > a');
     const tags = Array.prototype.map.call(tagListEl, item => item.textContent);
-    
+
     let imgSaveName = shortid.generate();
-    
+
     data.heading = heading;
     data.cat = categories;
     data.imgPath = path.join(__dirname, 'images', imgSaveName.concat(contImgExt));
@@ -110,47 +110,74 @@ const scrapePage = (htmlBody) => {
             throw err;
         })
         .pipe(fs.createWriteStream(data.imgPath));
-    
+
     return data;
 
 };
 
 
 // const planNextReq = async (url, cb, cookies, delay) => {
-    // delay = delay||timeout;
-    // setTimeout(() => {
-       // let resCookies = await nextReq(url, cookies);
-       // cb();
-    // }, delay);
+// delay = delay||timeout;
+// setTimeout(() => {
+// let resCookies = await nextReq(url, cookies);
+// cb();
+// }, delay);
 // };
 
 
 
-const nextReq = (currUrl, cookies) => {
-    
-    return new Promise( (resolve, reject) => {
-        
+const nextReq = (currUrl, cookieJar) => {
+
+    return new Promise((resolve, reject) => {
+
         if (!currUrl)
             reject('Не передан адрес страницы для парсинга');
         let pageId = currUrl.replace(siteAddr, '').split('/')[0];
 
-        let reqOpts = {
+        let headers = {
+            "Host": "scanlibs.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "ru,en-US;q=0.7,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "DNT": 1,
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": 1,
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+            "TE": "Trailers"
+        };
+
+        // let reqOpts = {
+        //     timeout: 10000,
+        //     followRedirect: true,
+        //     jar: (cookieJar || request.jar()),
+        //     headers: headers
+        // };
+
+        let jar = cookieJar ? cookieJar : request.jar();
+
+        //request.get(currUrl, reqOpts, (error, response, body) => {
+        request({
+            url: currUrl,
+            method: 'GET',
+            jar: jar,
             timeout: 10000,
             followRedirect: true,
-            jar: (cookies || request.jar()),
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
-        };
-        
-        request.get(currUrl, reqOpts, (error, response, body) => {
-            
+            headers: headers
+        }, (error, response, body) => {
             try {
-                if (error)
+                if (error) {
+                    console.log('Ошибка! Нет ответа по адресу ' + currUrl);
                     throw error;
+                }
                 if (response.statusCode !== 200)
                     throw new Error('error getting resource ' + currUrl);
-                
+
+
+
                 let savePath = path.join(__dirname, 'pages', pageId.concat('.html'));
-                
+
                 fs.writeFile(savePath, body, (err) => {
                     if (err) console.log(error);
                 });
@@ -165,26 +192,19 @@ const nextReq = (currUrl, cookies) => {
                 fs.writeFile(savePath, JSON.stringify(pageData), (err) => {
                     if (err) throw error;
                 });
-
-                if (!response.headers["set-cookie"]) {
-                    console.log("Нет новых кук от сервера");
-                    resolve(reqOpts.jar);  
-                }
-                else
-                    resolve(response.headers["set-cookie"]);
-
             } catch (err) {
                 console.log(err);
-                resolve(null);
+            } finally {
+                resolve(jar);
             }
-            
+
         }); //request.get
     }); //Promise
-    
+
 }; //nextReq
 
 const requestor = () => {
-    let cookies;
+    let cookieJar;
     lineReader.eachLine('scanlib-missed.txt', function(line, last, cb) {
         console.log(line);
         if (last) {
@@ -194,14 +214,18 @@ const requestor = () => {
             cb();
         else {
             let nextUrl = line.trim();
-            setTimeout( async () => {
-               cookies = await nextReq(url, cookies);
-               cb();
-            }, delay);
+            setTimeout(async () => {
+                try {
+                    cookieJar = await nextReq(nextUrl, cookieJar);
+                } catch (err) {
+                    console.log(err);
+                } finally {
+                    cb();
+                }
+            }, timeout);
         }
     });
 };
 
 // старт
 requestor();
-
