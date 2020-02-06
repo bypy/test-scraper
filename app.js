7ce01fb9db1e66e46c4fd927dfd97a8a34c8af0c
Alexei <mail@mail.by>
2020-02-06 16:57:17 +0300

    режим отладки парсинга страницы из локального файла

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const { JSDOM } = jsdom;
 const pagesPath = path.join(__dirname, 'pages');
 const imgPath = path.join(__dirname, 'images');
 const dataPath = path.join(__dirname, 'data');
+const siteAddr = 'https://scanlibs.com/'
+const reqTimeout = 4000 + 4000 * Math.random();
 
 [pagesPath, imgPath, dataPath].forEach(pth => {
     if (!fs.existsSync(pth)) {
@@ -16,17 +18,31 @@ const dataPath = path.join(__dirname, 'data');
     }
 })
 
-const siteAddr = 'https://scanlibs.com/'
-const timeout = 4000 + 4000 * Math.random();
 
-// let urls = [
-// 'https://scanlibs.com/learning-svg/',
-// 'https://scanlibs.com/jump-start-sass/'
-// ];
+const extractBodyTag = resBody => {
+    const html = resBody.match(/<body.*?>.+<\/body>/s)[0].replace(/<script.*?>[\s\S]*?<\/script>/gs, "");
+    const htmlDom = new JSDOM(html);
+    return htmlDom.window.document.body;
+};
 
 
 const scrapePage = (htmlBody) => {
     let data = {};
+
+    const procVideoInfo = pipedInfoString => {
+        const cleanInfo = [];
+        const infoArr = pipedInfoString.replace(/\n/g,'').replace(/\n/g,'').replace(/<br\/?>/gm,' | ').split('|');
+        infoArr.forEach(item => {
+            if (/Skill level/i.test(item)) {
+                data.skill = item.split(':')[1].trim();
+                let indexToRemove = infoArr.indexOf(item);
+                infoArr.splice(indexToRemove, 1);
+            } else {
+                cleanInfo.push(item.trim());
+            }
+        })
+        return cleanInfo;
+    };
 
     // Extracting data
     const heading = htmlBody.querySelector('h1').textContent;
@@ -38,29 +54,35 @@ const scrapePage = (htmlBody) => {
         .filter(item => (item));
 
     let imgSrc = htmlBody.querySelector('.entry-content > img').getAttribute('src');
-    let contImgExt = ""; // изначально расширение изображения нам неизвестно
+    let imgExt = ""; // изначально расширение изображения нам неизвестно
 
     if (/\.jpe?g$/.test(imgSrc))
-        contImgExt = '.jpg';
+        imgExt = '.jpg';
     else if (/\.png$/.test(imgSrc))
-        contImgExt = '.png';
+        imgExt = '.png';
     else if (/\.webp$/.test(imgSrc))
-        contImgExt = '.webp';
+        imgExt = '.webp';
 
     const container = htmlBody.querySelector('.entry-content');
     if (!container) throw new Error('Data containin element is missing');
     const info = {};
     const description = [];
-    const descriptionEl = container.querySelector('p');
 
+    const dlBtnDiv = container.querySelector('a[href="#download"]').parentElement;
+    const descrPars = container.querySelectorAll('p');
+    const descrLists = container.querySelectorAll('ul');
+    const stopperElem = container.querySelector('#download');
 
     const chldNodes = container.childNodes;
     let counter = 0;
+    let aboveDlBtn = true;
     while (counter < chldNodes.length) {
         let curr = chldNodes[counter];
-        if (curr === descriptionEl)
+        if (curr === stopperElem)
             break;
-        if (curr.nodeType === 3 && curr.textContent.trim() !== '') {
+        if (curr === dlBtnDiv)
+            aboveDlBtn = false;
+        if (curr.nodeType === 3 && curr.textContent.trim() !== '' && aboveDlBtn) {
             let key = '';
             let value = '';
             let properties = curr.textContent.trim().split(':');
@@ -69,11 +91,19 @@ const scrapePage = (htmlBody) => {
                 value = properties.slice(1).join(':').trim(); // если значение в свою очередь само имело знак :
             }
             info[key] = value;
+        } else if (descrPars.indexOf(curr) !== -1) {
+            if (!aboveDlBtn) {
+                description.push({p: curr});
+            } else {
+                info.video = procVideoInfo(curr.innerHTML);
+            } 
+        } else if (descrLists.indexOf(curr) !== -1) {
+            description.push({ul: curr});
         }
         counter++;
     }
 
-    const download = container.querySelector('#download');
+    
 
     if (descriptionEl) {
         description.push(descriptionEl.innerHTML);
@@ -98,7 +128,7 @@ const scrapePage = (htmlBody) => {
 
     data.heading = heading;
     data.cat = categories;
-    data.imgPath = path.join(__dirname, 'images', imgSaveName.concat(contImgExt));
+    data.imgPath = path.join(__dirname, 'images', imgSaveName.concat(imgExt));
     data.info = info;
     data.description = description;
     data.tags = tags;
@@ -114,16 +144,6 @@ const scrapePage = (htmlBody) => {
     return data;
 
 };
-
-
-// const planNextReq = async (url, cb, cookies, delay) => {
-// delay = delay||timeout;
-// setTimeout(() => {
-// let resCookies = await nextReq(url, cookies);
-// cb();
-// }, delay);
-// };
-
 
 
 const nextReq = (currUrl, cookieJar) => {
@@ -148,16 +168,8 @@ const nextReq = (currUrl, cookieJar) => {
             "TE": "Trailers"
         };
 
-        // let reqOpts = {
-        //     timeout: 10000,
-        //     followRedirect: true,
-        //     jar: (cookieJar || request.jar()),
-        //     headers: headers
-        // };
-
         let jar = cookieJar ? cookieJar : request.jar();
 
-        //request.get(currUrl, reqOpts, (error, response, body) => {
         request({
             url: currUrl,
             method: 'GET',
@@ -174,19 +186,14 @@ const nextReq = (currUrl, cookieJar) => {
                 if (response.statusCode !== 200)
                     throw new Error('error getting resource ' + currUrl);
 
-
-
                 let savePath = path.join(__dirname, 'pages', pageId.concat('.html'));
 
                 fs.writeFile(savePath, body, (err) => {
                     if (err) console.log(error);
                 });
-                const html = body.match(/<body.*?>.+<\/body>/s)[0].replace(/<script.*?>[\s\S]*?<\/script>/gs, "");
-                const htmlDom = new JSDOM(html);
-                const htmlBody = htmlDom.window.document.body;
 
-                const pageData = scrapePage(htmlBody);
-                //pageData.origName = pageId;
+                let htmlBody = extractBodyTag(body)
+                let pageData = scrapePage(htmlBody);
 
                 savePath = path.join(__dirname, 'data', pageId.concat('.json'));
                 fs.writeFile(savePath, JSON.stringify(pageData), (err) => {
@@ -199,13 +206,48 @@ const nextReq = (currUrl, cookieJar) => {
             }
 
         }); //request.get
-    }); //Promise
+    }); // Promise
 
-}; //nextReq
+}; // nextReq
+
+
+const isExists = targPath => {
+    fs.access(targPath, fs.F_OK, (err) => {
+        if (err) {
+            console.log(err)
+            return Promise.reject();
+        } else {
+            return Promise.resolve();
+        }
+    })
+};
+
+
+const readLocal = path => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // проверка на наличие файла
+            await isExists(path);
+            fs.readFile(path, 'utf8', (err, data) => {
+                if (err) {
+                    throw err;
+                } else {
+                    let htmlBody = extractBodyTag(data)
+                    let pageData = scrapePage(htmlBody);
+                    resolve(pageData);
+                }
+            })
+        } catch(err) {
+            console.log(err);
+            reject();
+        }
+    }); // Promise
+}; // readLocal
+
 
 const requestor = () => {
     let cookieJar;
-    lineReader.eachLine('scanlib-missed.txt', function(line, last, cb) {
+    lineReader.eachLine('scanlib-missed.txt', function (line, last, cb) {
         console.log(line);
         if (last) {
             cb(false); // stop reading
@@ -213,16 +255,18 @@ const requestor = () => {
         if (line.trim() === '')
             cb();
         else {
-            let nextUrl = line.trim();
+            let nextTarget = line.trim();
             setTimeout(async () => {
                 try {
-                    cookieJar = await nextReq(nextUrl, cookieJar);
+                    //cookieJar = await nextReq(nextTarget, cookieJar);
+                    let parsedData = await readLocal(nextTarget);
+                    console.log(parsedData);
                 } catch (err) {
                     console.log(err);
                 } finally {
                     cb();
                 }
-            }, timeout);
+            }, reqTimeout);
         }
     });
 };
